5d9c4285f4fa95afb5d873db5d625e691fc49972
mrana17 <rm_a@msn.com>
2021-04-05 14:07:28 +0200

    ✨ Create Order Card Component - Add CSS - Add images - Change `global.css` color naming

---
+++ components/orderCards/orderCards.tsx
@@ -0,0 +1,43 @@
+import styles from "./orderCards.module.css";
+
+export type OrderCardProps = {
+  imgSrc: string;
+  imgSrcStart: string;
+  imgSrcEnd: string;
+  name: string;
+  surname: string;
+  startAddress: string;
+  address: [string, number];
+};
+
+function OrderCard({
+  imgSrc,
+  imgSrcStart,
+  imgSrcEnd,
+  name,
+  surname,
+  startAddress,
+  address,
+  ...props
+}: OrderCardProps) {
+  return (
+    <article className={styles.orderCard} {...props}>
+      <div className={styles.orderCardContainer}>
+        <img src={imgSrc} className={styles.orderCardImg} />
+        <p className={styles.orderCardName}>
+          {name} {surname}
+        </p>
+        <p className={styles.orderCardStartAddress}>
+          <img src={imgSrcStart} className={styles.orderCardStart} />
+          {startAddress}
+        </p>
+        <p className={styles.orderCardAddress}>
+          <img src={imgSrcEnd} className={styles.orderCardEnd} />
+          {address}
+        </p>
+      </div>
+    </article>
+  );
+}
+
+export default OrderCard;
