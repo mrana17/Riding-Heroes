1b44f2b8c3abd2a82be788072d58a3192702704b
mrana17 <rm_a@msn.com>
2021-04-05 20:01:12 +0200

    ✨ Create Wallet Table Component
    - will be used on Wallet page
    - add css
    - placeholder data
    - install react-table

---
+++ components/table/table.tsx
@@ -0,0 +1,48 @@
+import styles from "./table.module.css";
+
+export type TableProps = {
+  idTitle: string;
+  addressTitle: string;
+  saleTitle: string;
+  id: string;
+  address: string;
+  sale: string;
+};
+
+function Table({
+  id,
+  address,
+  sale,
+  idTitle,
+  addressTitle,
+  saleTitle,
+}: TableProps) {
+  return (
+    <table className={styles.table}>
+      <tbody>
+        <tr>
+          <td className={styles.table}>{idTitle}</td>
+          <td className={styles.table}>{addressTitle}</td>
+          <td className={styles.table}>{saleTitle}</td>
+        </tr>
+        <tr>
+          <td className={styles.table}>{id}</td>
+          <td className={styles.table}>{address}</td>
+          <td className={styles.table}>{sale}</td>
+        </tr>
+        <tr>
+          <td className={styles.table}>{id}</td>
+          <td className={styles.table}>{address}</td>
+          <td className={styles.table}>{sale}</td>
+        </tr>
+        <tr>
+          <td className={styles.table}>{id}</td>
+          <td className={styles.table}>{address}</td>
+          <td className={styles.table}>{sale}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
