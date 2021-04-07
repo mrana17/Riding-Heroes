8a7aa8c941ddf365e3c7cbfd04f24fc2af26842e
mrana17 <rm_a@msn.com>
2021-04-07 16:43:05 +0200

    ♻️ changes in Choose Ride Component
    - style changes
    - classname changes

--- components/chooseRideButton/chooseRideButton.stories.tsx
+++ components/chooseRideButton/chooseRideButton.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import ChooseRideButton, { ChooseRideButtonProps } from "./chooseRideButton";
+import styles from "./chooseRideButton.module.css";
 
 export default {
   title: "Common/chooseRideButton",
@@ -15,19 +16,23 @@ export const CarButton = Template.bind({});
 CarButton.args = {
   imgSrc: "/car.svg",
   label: "Auto",
+  className: styles.chooseRideButton,
 };
 export const CycleButton = Template.bind({});
 CycleButton.args = {
   imgSrc: "/bike.svg",
   label: "Fahrrad",
+  className: styles.chooseRideButton,
 };
 export const ScooterButton = Template.bind({});
 ScooterButton.args = {
   imgSrc: "/scooter.svg",
   label: "Scooter",
+  className: styles.chooseRideButton,
 };
 export const RollerButton = Template.bind({});
 RollerButton.args = {
   imgSrc: "/Roller.svg",
   label: "Roller",
+  className: styles.chooseRideButton,
 };
