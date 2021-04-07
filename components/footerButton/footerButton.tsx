6a148bb55dd589bad8ebecb00093094550f4963b
mrana17 <rm_a@msn.com>
2021-04-07 16:43:45 +0200

    ♻️ ♻️ changes in Footer Button Component
    - style changes
    - classname changes

--- components/footerButton/footerButton.tsx
+++ components/footerButton/footerButton.tsx
@@ -1,27 +1,19 @@
-import { MouseEventHandler } from "react";
 import styles from "./footerButton.module.css";
 import Link from "next/link";
 
 export type FooterButtonProps = {
   label: string;
-  onClick: MouseEventHandler<HTMLButtonElement>;
   imgSrc: string;
   href: string;
 };
 
-function FooterButton({
-  label,
-  onClick,
-  imgSrc,
-  href,
-  ...props
-}: FooterButtonProps) {
+function FooterButton({ label, imgSrc, href, ...props }: FooterButtonProps) {
   return (
     <Link href={href}>
-      <button className={styles.footerButton} onClick={onClick} {...props}>
+      <a className={styles.footerButton} {...props}>
         <img src={imgSrc} className={styles.footerImg} />
         <label className={styles.footerBtnText}> {label}</label>
-      </button>
+      </a>
     </Link>
   );
 }
