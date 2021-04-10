1a389a5da5cdc392875b458cce3c5900668131ac
mrana17 <rm_a@msn.com>
2021-04-11 01:34:54 +0200

    ⚡️ Add Footer to Map Page

--- pages/MapPage.tsx
+++ pages/MapPage.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import ReactMapGL from "react-map-gl";
+import FooterButton from "../components/footerButton/footerButton";
+import styles from "../styles/MapPage.module.css";
 
 export default function MapPage() {
   const [viewport, setViewport] = useState({
@@ -10,11 +12,27 @@ export default function MapPage() {
     zoom: 10,
   });
   return (
-    <ReactMapGL
-      mapStyle="mapbox://styles/mapbox/streets-v11"
-      mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
-      {...viewport}
-      onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    ></ReactMapGL>
+    <>
+      <ReactMapGL
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
+        {...viewport}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      ></ReactMapGL>
+      <div className={styles.footer}>
+        <FooterButton
+          label="Profile"
+          imgSrc="/Profile-picture.svg"
+          href="/ProfilePage"
+        />
+        <FooterButton label="Wallet" imgSrc="/wallet.svg" href="WalletPage" />
+        <FooterButton label="Map" imgSrc="/route-map.svg" href="MapPage" />
+        <FooterButton
+          label="History"
+          imgSrc="/History.svg"
+          href="HistoryPage"
+        />
+      </div>
+    </>
   );
 }
